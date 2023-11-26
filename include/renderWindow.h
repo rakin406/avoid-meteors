4b7c0b49f49cf3ffce568a5a28ffab5b02f69979
rakin406 <rakinhacker@gmail.com>
2023-11-26 22:41:58 +0600

    Add renderWindow header and implementation

---
+++ include/renderWindow.h
@@ -0,0 +1,45 @@
+#ifndef RENDER_WINDOW_H
+#define RENDER_WINDOW_H
+
+#include <SDL2/SDL.h>
+
+#include <string_view>
+
+class RenderWindow
+{
+public:
+    RenderWindow(std::string_view title, int width, int height);
+
+    /**
+     * @brief Clear screen with background color. Default is white.
+     *
+     * @param red Default = 255
+     * @param green Default = 255
+     * @param blue Default = 255
+     */
+    void clear(Uint8 red = 255, Uint8 green = 255, Uint8 blue = 255);
+
+    /**
+     * @brief Load texture from image.
+     *
+     * @param filePath Image
+     * @return texture
+     */
+    SDL_Texture* loadTexture(std::string_view filePath);
+
+    /**
+     * @brief Display screen.
+     */
+    void display();
+
+    /**
+     * @brief Kill and exit window.
+     */
+    void destroy();
+
+private:
+    SDL_Window* window { nullptr };
+    SDL_Renderer* renderer { nullptr };
+};
+
+#endif
---
+++ src/renderWindow.cpp
@@ -0,0 +1,76 @@
+#include "renderWindow.h"
+
+#include <SDL2/SDL.h>
+#include <SDL_image.h>
+
+#include <iostream>
+#include <string_view>
+
+RenderWindow::RenderWindow(std::string_view title, int width, int height)
+{
+    window = SDL_CreateWindow(title.data(), SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED, width, height,
+                              SDL_WINDOW_SHOWN);
+
+    if (window == nullptr)
+    {
+        std::cout << "Window failed to init. Error: " << SDL_GetError()
+                  << std::endl;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
+
+// TODO: Put this function somewhere else.
+/**
+ * @brief Return true if user requests quit. For use in main loop.
+ *
+ * @return boolean.
+ */
+// bool isQuitRequested()
+//{
+//     SDL_Event event {};
+//
+//     // Check for events
+//     while (SDL_PollEvent(&event) != 0)
+//     {
+//         if (event.type == SDL_QUIT)
+//             return true;
+//
+//         if (event.type == SDL_KEYDOWN)
+//         {
+//             if (event.key.keysym.sym == SDLK_ESCAPE)
+//                 return true;
+//         }
+//     }
+//
+//     return false;
+// }
+
+void RenderWindow::clear(Uint8 red = 255, Uint8 green = 255, Uint8 blue = 255)
+{
+    SDL_SetRenderDrawColor(renderer, red, green, blue, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+}
+
+SDL_Texture* RenderWindow::loadTexture(std::string_view filePath)
+{
+    SDL_Texture* texture { IMG_LoadTexture(renderer, filePath.data()) };
+
+    if (texture == nullptr)
+    {
+        std::cout << "Failed to load texture. Error: " << SDL_GetError()
+                  << std::endl;
+    }
+
+    return texture;
+}
+
+void RenderWindow::display() { SDL_RenderPresent(renderer); }
+
+void RenderWindow::destroy()
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
