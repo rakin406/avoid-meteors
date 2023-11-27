d2e5b5777f6031e22adf460d678d33df8c0d0f2a
rakin406 <rakinhacker@gmail.com>
2023-11-27 18:53:14 +0600

    Load texture from background image

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,64 @@
 #include "components.h"
+#include "renderWindow.h"
 #include "tags.h"
 
+#include "SDL.h"
+#include "SDL_image.h"
 #include <flecs.h>
 
-#include <iostream>
+namespace
+{
+    /**
+     * @brief Return true if user requests quit. For use in main loop.
+     *
+     * @param event SDL_Event&
+     * @return bool
+     */
+    bool isQuitRequested(const SDL_Event& event)
+    {
+        if (event.type == SDL_QUIT)
+        {
+            return true;
+        }
+        else if (event.type == SDL_KEYDOWN)
+        {
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+                return true;
+        }
+
+        return false;
+    }
+} // namespace
 
 int main()
 {
-    flecs::world world;
+    // flecs::world world;
 
-    auto player = world.entity();
-    player.add<Player>();
+    // auto player = world.entity();
+    // player.add<Player>();
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
+
+    RenderWindow window { "Avoid Meteors", 1000, 746 };
+    SDL_Texture* bgTexture = window.loadTexture("res/gfx/background.jpg");
+
+    bool gameRunning { true };
+    SDL_Event event {};
+
+    while (gameRunning)
+    {
+        // Get our controls and events
+        while (SDL_PollEvent(&event))
+        {
+            if (isQuitRequested(event))
+                gameRunning = false;
+        }
+
+        window.display();
+    }
+
+    window.destroy();
 
     return 0;
 }
