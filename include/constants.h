0375a48b0edac7a6bff44b31faa888ddaa12a3d3
rakin406 <rakinhacker@gmail.com>
2023-11-28 19:30:53 +0600

    Add idle and running sprite constants

--- include/constants.h
+++ include/constants.h
@@ -7,8 +7,13 @@ namespace constants
 {
     inline constexpr int WINDOW_WIDTH { 1280 };
     inline constexpr int WINDOW_HEIGHT { 720 };
+
     inline constexpr std::string_view BG_IMG_PATH { PROJECT_ROOT
                                                     "res/gfx/background.jpg" };
+    inline constexpr std::string_view IDLE_SPRITE { PROJECT_ROOT
+                                                    "res/gfx/player/idle.png" };
+    inline constexpr std::string_view RUNNING_SPRITE { PROJECT_ROOT
+                                                    "res/gfx/player/running.png" };
 } // namespace constants
 
 #endif
