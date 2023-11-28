d126e02bab882f8e13531fedb976eb56d36842f4
rakin406 <rakinhacker@gmail.com>
2023-11-28 12:22:57 +0600

    Add velocity component shortcut

--- include/components.h
+++ include/components.h
@@ -2,10 +2,12 @@
 #define COMPONENTS_H
 
 #include "../deps/flecs_components_transform.h"
+#include "../deps/flecs_components_physics.h"
 
 using namespace flecs::components;
 
 // Shortcuts to imported components
 using Position = transform::Position2;
+using Velocity = physics::Velocity2;
 
 #endif
