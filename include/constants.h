5d83a0823bde258df8ca1fa34aec721b17374839
rakin406 <rakinhacker@gmail.com>
2023-12-01 01:13:59 +0600

    Add player sheet path constant

--- include/constants.h
+++ include/constants.h
@@ -10,18 +10,18 @@ namespace constants
 {
     inline constexpr int WINDOW_WIDTH { 1280 };
     inline constexpr int WINDOW_HEIGHT { 720 };
+    inline constexpr int GROUND_POS_Y { WINDOW_HEIGHT - 10 };
+    inline constexpr int PLAYER_SPEED { 1 };
 
     inline constexpr std::string_view BG_IMG_PATH { PROJECT_ROOT
                                                     "res/gfx/background.jpg" };
-
-    // Based off of background texture ground
-    inline constexpr int GROUND_POS_Y { WINDOW_HEIGHT - 10 };
+    inline constexpr std::string_view PLAYER_SHEET_PATH {
+        PROJECT_ROOT "res/gfx/player.png"
+    };
 
     inline constexpr std::array<Direction, 2> ALL_DIRECTIONS {
         Direction::Left, Direction::Right
     };
-
-    inline constexpr int PLAYER_SPEED { 1 };
 } // namespace constants
 
 #endif
