5ed67ceedda5f43f02f64d103c265f43c90ef990
rakin406 <rakinhacker@gmail.com>
2023-12-01 16:25:55 +0600

    increase speed

--- include/constants.h
+++ include/constants.h
@@ -11,7 +11,7 @@ namespace constants
     inline constexpr int WINDOW_WIDTH { 1280 };
     inline constexpr int WINDOW_HEIGHT { 720 };
     inline constexpr int GROUND_POS_Y { WINDOW_HEIGHT - 10 };
-    inline constexpr int PLAYER_SPEED { 1 };
+    inline constexpr int PLAYER_SPEED { 10 };
 
     inline constexpr std::string_view BG_IMG_PATH { PROJECT_ROOT
                                                     "res/gfx/background.jpg" };
