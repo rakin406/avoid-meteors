e4b327b714b098af6b4638ea87d32c0eb1798cf6
rakin406 <rakinhacker@gmail.com>
2023-12-04 20:22:40 +0600

    Finish clock class

--- include/clock.h
+++ include/clock.h
@@ -1,10 +1,27 @@
 #ifndef CLOCK_H
 #define CLOCK_H
 
+#include <chrono>
+
 class Clock
 {
 public:
+    Clock();
+
+    /**
+     * @brief Updates clock one tick.
+     */
+    void tick();
+
+    /**
+     * @brief Gets delta time.
+     * @return delta time.
+     */
+    float getDeltaTime() const;
+
 private:
+    std::chrono::high_resolution_clock::time_point lastTick {};
+    float deltaTime {};
 };
 
 #endif
--- src/clock.cpp
+++ src/clock.cpp
@@ -1,1 +1,20 @@
 #include "clock.h"
+
+#include <chrono>
+
+Clock::Clock()
+    : lastTick { std::chrono::high_resolution_clock::now() }, deltaTime { 0 }
+{
+}
+
+void Clock::tick()
+{
+    using namespace std::chrono;
+
+    auto now { high_resolution_clock::now() };
+    duration<float> elapsed { lastTick - now };
+    deltaTime = elapsed.count();
+    lastTick = now;
+}
+
+float Clock::getDeltaTime() const { return deltaTime; }
