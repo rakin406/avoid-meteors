41bc9e38c42822f07072363c4ad749828fea2300
rakin406 <rakinhacker@gmail.com>
2023-12-05 13:20:38 +0600

    Create modules folder

---
+++ include/modules/player.h
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * @brief Provides player module.
+ */
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "../tags.h"
+
+namespace modules
+{
+    /**
+     * @brief Player module.
+     */
+    class Player
+    {
+    public:
+    private:
+    };
+} // namespace modules
+
+#endif
