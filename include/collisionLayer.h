b4eabb40b0fac43a7e00f7de7fcbaf8644bbb111
rakin406 <rakinhacker@gmail.com>
2023-12-07 22:45:47 +0600

    Remove enumflags and add collisionlayer

---
+++ include/collisionLayer.h
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * @brief Provides collision layer for the entities.
+ */
+
+#ifndef COLLISION_LAYER_H
+#define COLLISION_LAYER_H
+
+struct CollisionLayer
+{
+    struct Barrier
+    {
+    };
+
+    struct Ground
+    {
+    };
+
+    struct Meteor
+    {
+    };
+
+    struct Player
+    {
+    };
+};
+
+#endif
