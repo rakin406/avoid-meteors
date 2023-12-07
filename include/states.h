0f476cd00965afff40ba2bab488002689f05bb16
rakin406 <rakinhacker@gmail.com>
2023-12-07 17:18:01 +0600

    Add flags for collision

--- include/states.h
+++ include/states.h
@@ -6,6 +6,14 @@
 #ifndef STATES_H
 #define STATES_H
 
+enum class CollisionFlags
+{
+    Barrier = 1 << 0,
+    Ground = 1 << 1,
+    Meteor = 1 << 2,
+    Player = 1 << 3
+};
+
 enum class Movement
 {
     Idle,
