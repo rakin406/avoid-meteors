6b75dced01c0110c1d7c78bd9abd8b9588e88a68
rakin406 <rakinhacker@gmail.com>
2023-12-09 11:53:56 +0600

    Clean up includes

--- src/game.cpp
+++ src/game.cpp
@@ -1,13 +1,10 @@
 #include "game.h"
 #include "collisionLayer.h"
-#include "colors.h"
 #include "components.h"
-#include "constants.h"
 #include "modules/player.h"
-#include "renderWindow.h"
+#include "modules/renderSystem.h"
 #include "tags.h"
 
-#include "SDL.h"
 #include <flecs.h>
 
 #include <iostream>
@@ -23,18 +20,10 @@ void Game::run()
 
 void Game::init()
 {
-    using namespace constants;
     using namespace modules;
 
+    world.import <RenderSystem>();
     world.import <Player>();
-
-    // System that loads background texture on startup
-    world.system<RenderWindow, Sprite, Background>("LoadBackground")
-        .kind(flecs::OnStart)
-        .term_at(1)
-        .singleton()
-        .each([](RenderWindow& window, Sprite& sprite, Background)
-              { sprite.texture = window.loadTexture(assets::BACKGROUND); });
 
     // System that checks for collisions between entities
     world.system<Collider>("Collision")
@@ -48,71 +37,11 @@ void Game::init()
                     // NOTE: I have no idea how the hell this is working...
                     if (transform->position.x <= -Player::FRAME_SIZE ||
                         transform->position.x >=
-                            (window::WIDTH -
+                            (RenderSystem::WINDOW_WIDTH -
                              (Player::FRAME_SIZE * (Player::FRAME_SCALE - 1))))
                     {
                         entity.add<CollidesWith, CollisionLayer::Wall>();
                     }
                 }
-            });
-
-    auto spriteRendererSystem {
-        world
-            .system<const Transform, const Sprite, RenderWindow,
-                    SpriteRenderer>("SpriteRendererSystem")
-            .kind(0)
-            .term_at(3)
-            .singleton()
-            .each(
-                [](flecs::entity entity, const Transform& transform,
-                   const Sprite& sprite, RenderWindow& window, SpriteRenderer)
-                {
-                    // Render background
-                    if (entity.has<Background>())
-                    {
-                        window.render(sprite.texture, nullptr, nullptr, 0,
-                                      nullptr, SDL_FLIP_NONE, sprite.color);
-                    }
-                    // Render player
-                    else if (entity.has<Player::PlayerTag>() &&
-                             entity.has<Player::Animation>())
-                    {
-                        Player::Animation* animation {
-                            entity.get_mut<Player::Animation>()
-                        };
-                        SDL_FRect dest {
-                            transform.position.x, transform.position.y,
-                            animation->frameRec.w * transform.scale.x,
-                            animation->frameRec.h * transform.scale.y
-                        };
-                        window.render(sprite.texture, &animation->frameRec,
-                                      &dest, transform.rotation, nullptr,
-                                      animation->flip, sprite.color);
-                    }
-                })
-    };
-
-    // System that updates and renders game
-    world.system<SDL_Event, RenderWindow>("UpdateWindow")
-        .kind(flecs::PostUpdate)
-        .term_at(1)
-        .singleton()
-        .term_at(2)
-        .singleton()
-        .each(
-            [=](SDL_Event& event, RenderWindow& window)
-            {
-                // Get our controls and events
-                while (SDL_PollEvent(&event))
-                {
-                    if (isQuitRequested(event))
-                        world.quit();
-                }
-
-                window.clear(WHITE);
-
-                spriteRendererSystem.run();
-
-                window.display();
             });
 }
--- src/modules/renderSystem.cpp
+++ src/modules/renderSystem.cpp
@@ -1,4 +1,6 @@
 #include "modules/renderSystem.h"
+#include "modules/player.h"
+#include "colors.h"
 #include "components.h"
 #include "renderWindow.h"
 #include "tags.h"
@@ -31,7 +33,79 @@ namespace
 
 } // namespace
 
-modules::RenderSystem::RenderSystem(flecs::world& world) {}
+modules::RenderSystem::RenderSystem(flecs::world& world)
+{
+    // Setup window
+    windowInit(world);
+
+    // System that loads background texture on startup
+    world.system<RenderWindow, Sprite, Background>("LoadBackground")
+        .kind(flecs::OnStart)
+        .term_at(1)
+        .singleton()
+        .each([](RenderWindow& window, Sprite& sprite, Background)
+              { sprite.texture = window.loadTexture(BACKGROUND); });
+
+    auto spriteRendererSystem {
+        world
+            .system<const Transform, const Sprite, RenderWindow,
+                    SpriteRenderer>("SpriteRendererSystem")
+            .kind(0)
+            .term_at(3)
+            .singleton()
+            .each(
+                [](flecs::entity entity, const Transform& transform,
+                   const Sprite& sprite, RenderWindow& window, SpriteRenderer)
+                {
+                    // Render background
+                    if (entity.has<Background>())
+                    {
+                        window.render(sprite.texture, nullptr, nullptr, 0,
+                                      nullptr, SDL_FLIP_NONE, sprite.color);
+                    }
+                    // Render player
+                    else if (entity.has<Player::PlayerTag>() &&
+                             entity.has<Player::Animation>())
+                    {
+                        Player::Animation* animation {
+                            entity.get_mut<Player::Animation>()
+                        };
+                        SDL_FRect dest {
+                            transform.position.x, transform.position.y,
+                            animation->frameRec.w * transform.scale.x,
+                            animation->frameRec.h * transform.scale.y
+                        };
+                        window.render(sprite.texture, &animation->frameRec,
+                                      &dest, transform.rotation, nullptr,
+                                      animation->flip, sprite.color);
+                    }
+                })
+    };
+
+    // System that updates and renders game
+    world.system<SDL_Event, RenderWindow>("UpdateWindow")
+        .kind(flecs::PostUpdate)
+        .term_at(1)
+        .singleton()
+        .term_at(2)
+        .singleton()
+        .each(
+            [=](SDL_Event& event, RenderWindow& window)
+            {
+                // Get our controls and events
+                while (SDL_PollEvent(&event))
+                {
+                    if (isQuitRequested(event))
+                        world.quit();
+                }
+
+                window.clear(WHITE);
+
+                spriteRendererSystem.run();
+
+                window.display();
+            });
+}
 
 void modules::RenderSystem::windowInit(flecs::world& world)
 {
