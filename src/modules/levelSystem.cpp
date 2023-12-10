df02c4bc1eb14d69c6e2620d22b4c58e5ad0951c
rakin406 <rakinhacker@gmail.com>
2023-12-10 16:19:51 +0600

    Add function lines

--- src/modules/levelSystem.cpp
+++ src/modules/levelSystem.cpp
@@ -1,1 +1,10 @@
 #include "modules/levelSystem.h"
+
+#include <flecs.h>
+
+#include <iostream>
+
+// TODO: Complete constructor.
+modules::LevelSystem::LevelSystem(flecs::world& world) {}
+
+void modules::LevelSystem::levelInit(flecs::world& world) {}
