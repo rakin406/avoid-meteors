f9bad44e50bc9407d50514eee3f745a5d95f59be
rakin406 <rakinhacker@gmail.com>
2023-12-11 13:18:21 +0600

    Register Text singleton component

--- src/modules/scoreSystem.cpp
+++ src/modules/scoreSystem.cpp
@@ -10,12 +10,14 @@ modules::ScoreSystem::ScoreSystem(flecs::world& world)
 
     // Register components
     world.component<Score>();
+    world.component<Text>();
 
     scoreInit(world);
 }
 
 void modules::ScoreSystem::scoreInit(flecs::world& world)
 {
-    // Set singleton
+    // Set singletons
     world.set<Score>({ 0 });
+    world.set<Text>({ "Score: 0", FONT_SIZE, TEXT_COLOR, TEXT_POSITION });
 }
