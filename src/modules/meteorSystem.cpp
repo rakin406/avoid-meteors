553d4d7f5e7454b263d5cf9f64893af1a5f57ed7
rakin406 <rakinhacker@gmail.com>
2023-12-11 22:40:36 +0600

    Create meteor entities

--- src/modules/meteorSystem.cpp
+++ src/modules/meteorSystem.cpp
@@ -1,1 +1,38 @@
 #include "modules/meteorSystem.h"
+#include "collisionLayer.h"
+#include "components.h"
+#include "tags.h"
+
+#include <flecs.h>
+
+#include <iostream>
+#include <ranges>
+
+modules::MeteorSystem::MeteorSystem(flecs::world& world)
+{
+    world.module<MeteorSystem>();
+
+    // Register components
+    // world.component<Level>();
+
+    meteorsInit(world);
+}
+
+void modules::MeteorSystem::meteorsInit(flecs::world& world)
+{
+    const Level* level { world.get<Level>() };
+
+    for (int i : std::views::iota(0, level->numMeteors))
+    {
+        auto meteor { world.entity() };
+        meteor.add<Meteor>()
+            .add<CollisionLayer::Meteor>()
+            .add<CollisionMask::Ground>()
+            .add<CollisionMask::Player>()
+            .add<SpriteRenderer>()
+            .add<Sprite>()
+            // TODO: Set component values.
+            .set<Transform>({})
+            .set<Velocity>({});
+    }
+}
