10c2c4d19ed2c2eb16ae327a4e03f1a376df581d
rakin406 <rakinhacker@gmail.com>
2023-12-11 19:02:20 +0600

    Fix read access violation

--- src/tools.cpp
+++ src/tools.cpp
@@ -14,13 +14,13 @@ SDL_FPoint tools::getSize(SDL_Texture* texture)
 {
     SDL_FPoint size {};
 
-    // Can't directly get size since it's float. Use integer pointers.
-    int* width {};
-    int* height {};
-    SDL_QueryTexture(texture, nullptr, nullptr, width, height);
+    // Can't directly get size since it's float. Use integers.
+    int width {};
+    int height {};
+    SDL_QueryTexture(texture, nullptr, nullptr, &width, &height);
 
-    size.x = *width;
-    size.y = *height;
+    size.x = static_cast<float>(width);
+    size.y = static_cast<float>(height);
 
     return size;
 }
