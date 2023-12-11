dfcc9db3fe9f625b1a66ce6ed52f79dc7293349a
rakin406 <rakinhacker@gmail.com>
2023-12-11 13:43:29 +0600

    Create observer for score change

--- src/modules/scoreSystem.cpp
+++ src/modules/scoreSystem.cpp
@@ -2,6 +2,7 @@
 
 #include <flecs.h>
 
+#include <format>
 #include <iostream>
 
 modules::ScoreSystem::ScoreSystem(flecs::world& world)
@@ -13,6 +14,18 @@ modules::ScoreSystem::ScoreSystem(flecs::world& world)
     world.component<Text>();
 
     scoreInit(world);
+
+    // Observer to update text upon score change
+    world.observer<const Score>("Score Change")
+        .event(flecs::OnSet)
+        .term_at(1)
+        .singleton()
+        .each(
+            [](flecs::iter& it, const Score& score)
+            {
+                Text* text { it.world().get_mut<Text>() };
+                text->content = std::format("Score: {}", score);
+            });
 }
 
 void modules::ScoreSystem::scoreInit(flecs::world& world)
