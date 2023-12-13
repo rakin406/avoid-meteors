307fb4341b5f37398d0a7d66d3c716623ba16c84
rakin406 <rakinhacker@gmail.com>
2023-12-13 13:26:48 +0600

    Add direction unit vector constants

--- include/constants.h
+++ include/constants.h
@@ -6,6 +6,8 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#include <glm/glm.hpp>
+
 #include <string_view>
 
 inline constexpr int WINDOW_WIDTH { 1280 };
@@ -13,5 +15,9 @@ inline constexpr int WINDOW_HEIGHT { 720 };
 inline constexpr std::string_view WINDOW_TITLE { "Avoid Meteors" };
 
 inline constexpr int GROUND_POS_Y { WINDOW_HEIGHT - 195 };
+
+// Direction constants
+inline constexpr glm::vec2 LEFT { -1, 0 }; // Left unit vector
+inline constexpr glm::vec2 RIGHT { 1, 0 }; // Right unit vector
 
 #endif
