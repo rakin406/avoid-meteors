b3d16b6b42e66ea0ac9ee097f852d9acf328d93b
rakin406 <rakinhacker@gmail.com>
2023-12-14 11:27:37 +0600

    Project paused, goodbye

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # avoid-meteors
 A 2D game where you avoid the incoming meteors
+
+NOTE: Project paused.
--- include/components.h
+++ include/components.h
@@ -11,6 +11,7 @@
 
 #include <string_view>
 
+// TODO: Change this.
 using Direction = glm::vec2;
 using Velocity = glm::vec2;
 
